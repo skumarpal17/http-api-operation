c4cf7b0d4f171151acb2ae537b303626260ced1f
Sachin Pal <44772464+skumarpal17@users.noreply.github.com>
2022-06-26 14:13:22 +0530

    Update services.dart

--- lib/services.dart
+++ lib/services.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Services{
+  
+  // post and fetch
 
   Future<List<ModelClass>> getData() async {
     List<ModelClass> postlist = [];
